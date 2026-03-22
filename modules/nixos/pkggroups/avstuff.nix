4b4deeceff22ef9de33ea194de94c61b0d55ed97
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-03-23 00:56:40 +0100

    add back kdenlive

--- modules/nixos/pkggroups/avstuff.nix
+++ modules/nixos/pkggroups/avstuff.nix
@@ -16,7 +16,7 @@ in
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       obs-studio
-      # kdePackages.kdenlive
+      kdePackages.kdenlive
       audacity
     ];
   };
