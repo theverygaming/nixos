12ab1404da316a640538c2a0b964dca45bcc22e3
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-08-09 06:42:52 +0200

    atticradio: set up RTL-TCP for each SDR + set up rtl_433

--- hosts/atticradio/radio/default.nix
+++ hosts/atticradio/radio/default.nix
@@ -1,31 +1,38 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  flakeInputs,
+  ...
+}:
 
 {
   imports = [
     ./sdrscheduler.nix
   ];
 
+  sops.secrets.rtl_433_mqtt_host = {
+    sopsFile = flakeInputs.secrets + "/hosts/atticradio/rtl_433.yaml";
+    key = "mqtt_host";
+  };
+
+  sops.secrets.rtl_433_mqtt_port = {
+    sopsFile = flakeInputs.secrets + "/hosts/atticradio/rtl_433.yaml";
+    key = "mqtt_port";
+  };
+
+  sops.secrets.rtl_433_mqtt_username = {
+    sopsFile = flakeInputs.secrets + "/hosts/atticradio/rtl_433.yaml";
+    key = "mqtt_username";
+  };
+
+  sops.secrets.rtl_433_mqtt_password = {
+    sopsFile = flakeInputs.secrets + "/hosts/atticradio/rtl_433.yaml";
+    key = "mqtt_password";
+  };
+
   custom.sdr-scheduler = {
     enable = true;
     sdrs = {
-      "qfh" = {
-        /*
-          Realtek, RTL2838UHIDIR, nooelec nesdr SMArt
-          Antenna: 137Mhz QFH
-        */
-        type = "rtl-sdr";
-        serial = "37063354";
-      };
-
-      "logped" = {
-        /*
-          Realtek, RTL2838UHIDIR, generic, missing case (taped :P)
-          Antenna: logperiodic
-        */
-        type = "rtl-sdr";
-        serial = "89612941";
-      };
-
       "v4" = {
         /*
           RTLSDRBlog, Blog V4
@@ -35,48 +42,41 @@
         serial = "67604236";
         services = [
           {
-            type = "script";
-            priority = 0;
-            script = {
-              name = "rtl_433";
-              script = ''
-                ${pkgs.rtl_433}/bin/rtl_433 -f 433.92M -s 1.024M -d ":''${RTL_SERIAL}"
-              '';
-              runCondition = ''
-                if [ $((($(date +%s) / 60) % 2)) == 0 ]; then
-                  SHOULD_RUN=true
-                else
-                  SHOULD_RUN=false
-                fi
-              '';
-            };
-          }
-          {
-            type = "script_cron";
-            priority = 1;
-            script_cron = {
-              name = "kalibrate-rtl";
-              script = ''
-                DEVIDX="$(${pkgs.rtl-sdr}/bin/rtl_test -d -1 2>&1 | grep "''${RTL_SERIAL}" | ${pkgs.gawk}/bin/awk '{print $1}' | tr -d ':' | tr -d '[:space:]')"
-                # kalibrate-rtl needs HOME set, otherwise it'll crash :)
-                export HOME=/tmp
-                ${pkgs.kalibrate-rtl}/bin/kal -b GSM900 -c 5 -d "''${DEVIDX}"
-              '';
-              onCalendar = [
-                "*-*-* *:*:15"
-                "*-*-* *:*:45"
-              ];
-            };
-          }
-          {
             type = "rtl_tcp";
-            priority = 2;
+            priority = 100;
             rtl_tcp.port = 1234;
           }
+        ];
+      };
+
+      "qfh" = {
+        /*
+          Realtek, RTL2838UHIDIR, nooelec nesdr SMArt
+          Antenna: 137Mhz QFH
+        */
+        type = "rtl-sdr";
+        serial = "37063354";
+        services = [
           {
             type = "rtl_tcp";
-            priority = 3;
+            priority = 100;
             rtl_tcp.port = 1235;
+          }
+        ];
+      };
+
+      "logped" = {
+        /*
+          Realtek, RTL2838UHIDIR, generic, missing case (taped :P)
+          Antenna: logperiodic
+        */
+        type = "rtl-sdr";
+        serial = "89612941";
+        services = [
+          {
+            type = "rtl_tcp";
+            priority = 100;
+            rtl_tcp.port = 1236;
           }
         ];
       };
@@ -88,6 +88,56 @@
         */
         type = "rtl-sdr";
         serial = "72977076";
+        services = [
+          {
+            type = "script";
+            priority = 0;
+            script = {
+              name = "rtl_433";
+              script =
+                let
+                  rtl_433_mqtt_hass = pkgs.stdenv.mkDerivation {
+                    name = "rtl_433_mqtt_hass";
+                    src = pkgs.rtl_433.src;
+
+                    propagatedBuildInputs = [
+                      (pkgs.python3.withPackages (
+                        ps: with ps; [
+                          paho-mqtt
+                        ]
+                      ))
+                    ];
+
+                    installPhase = ''
+                      mkdir -p $out/bin
+                      cp examples/rtl_433_mqtt_hass.py $out/bin/rtl_433_mqtt_hass.py
+                    '';
+                  };
+                in
+                ''
+                  export MQTT_HOST="$(cat $CREDENTIALS_DIRECTORY/mqtt_host)"
+                  export MQTT_PORT="$(cat $CREDENTIALS_DIRECTORY/mqtt_port)"
+                  export MQTT_USERNAME="$(cat $CREDENTIALS_DIRECTORY/mqtt_username)"
+                  export MQTT_PASSWORD="$(cat $CREDENTIALS_DIRECTORY/mqtt_password)"
+                  ${rtl_433_mqtt_hass}/bin/rtl_433_mqtt_hass.py --host "$MQTT_HOST" --port "$MQTT_PORT" &
+                  ${pkgs.rtl_433}/bin/rtl_433 -f 433.92M -s 1.024M -F "mqtt://$MQTT_HOST:$MQTT_PORT" -F log -M level -g auto -d ":''${RTL_SERIAL}"
+                '';
+              extraServiceConfig = {
+                LoadCredential = [
+                  "mqtt_host:${config.sops.secrets.rtl_433_mqtt_host.path}"
+                  "mqtt_port:${config.sops.secrets.rtl_433_mqtt_port.path}"
+                  "mqtt_username:${config.sops.secrets.rtl_433_mqtt_username.path}"
+                  "mqtt_password:${config.sops.secrets.rtl_433_mqtt_password.path}"
+                ];
+              };
+            };
+          }
+          {
+            type = "rtl_tcp";
+            priority = 100;
+            rtl_tcp.port = 1237;
+          }
+        ];
       };
     };
   };
